1994c9ea85027e49ea3a028b7fffbd4c38181d47
Patrick Santos <patrickceg@users.noreply.github.com>
2018-03-30 16:21:46 -0400

    Add the code to parse the port list arguments

--- bannergrabtroll.go
+++ bannergrabtroll.go
@@ -2,14 +2,60 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"strconv"
+	"strings"
 )
+
+// unit16Ranges takes the input string and encodes it as 2d slices of uint16
+// this function is intended for lists of ports
+// Each "row" (the first value) in the 2D slice is 1 or 2 elements: a 1 element
+// slice is for single numbers, wheras a 2 element slice is used for ranges, where
+// the first element is the minimum and the last element is the maximum
+func unit16Ranges(portString string) ([][]uint16, error) {
+	allLists := strings.Split(portString, ",")
+	var toReturn [][]uint16
+	if len(allLists) > 0 {
+		for _, portSetString := range allLists {
+			ports := strings.Split(portSetString, ":")
+			portsLen := len(ports)
+			parsePortErrorMessage := "Could not parse port %s from %s"
+			if portsLen == 1 || portsLen == 2 {
+				minOrSingle, errLo := strconv.ParseUint(ports[0], 10, 16)
+				if errLo != nil {
+					return [][]uint16{}, fmt.Errorf(parsePortErrorMessage, ports[0], ports)
+				}
+				if portsLen == 2 {
+					max, errHi := strconv.ParseUint(ports[1], 10, 16)
+					if errHi != nil {
+						return [][]uint16{}, fmt.Errorf(parsePortErrorMessage, ports[1], ports)
+					}
+					if minOrSingle > max {
+						return [][]uint16{}, fmt.Errorf("Reversed range: %d should be less than %d", minOrSingle, max)
+					}
+					toReturn = append(toReturn, []uint16{uint16(minOrSingle), uint16(max)})
+				} else {
+					toReturn = append(toReturn, []uint16{uint16(minOrSingle)})
+				}
+			} else {
+				return [][]uint16{}, fmt.Errorf("Could not parse %s as a single port x or x:y range", ports)
+			}
+		}
+	} else {
+		return [][]uint16{}, fmt.Errorf("Error parsing port list from %s", portString)
+	}
+	return toReturn, nil
+}
 
 func main() {
 	var portString string
 	flag.StringVar(&portString, "ports", "8082:8085,9543", "Ports separated by comma, colon for ranges: 300:400,505 is for ports 300 to 400 plus port 505")
-	payloadKbytes := flag.Int("payloadkbytes", 64, "How many kilobytes to send as payload to a connection")
-	rateKbytesPerConnection := flag.Int("ratekbytesperconnection", 32, "Rate limit in kilobytes per second per connection")
-	numConnections := flag.Int("numconnections", 16, "Number of simultaneous connections across any ports")
+
+	// portRanges, err := unit16Ranges(portString)
+
+	//payloadKbytes := flag.Int("payloadkbytes", 64, "How many kilobytes to send as payload to a connection")
+	//rateKbytesPerConnection := flag.Int("ratekbytesperconnection", 32, "Rate limit in kilobytes per second per connection")
+	//numConnections := flag.Int("numconnections", 16, "Number of simultaneous connections across any ports")
 
 	// TODO: Validate ports list
 
---
+++ bannergrabtroll_test.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+// This method runs unit16Ranges against the input and compares it to the expected
+// This assumes no error should be thrown. For tests that should throw an error,
+// use assertionErrTest
+func assertionNoErrTest(input string, expected [][]uint16, t *testing.T) {
+	actual, err := unit16Ranges(input)
+	if err != nil {
+		t.Fatalf("Unexpected error %s", err)
+	}
+	if !reflect.DeepEqual(expected, actual) {
+		t.Fatalf("Expected %v but got %v", expected, actual)
+	}
+}
+
+// Test that assumes there is an error in the input string to unit16Ranges
+// (and fails if there is no error)
+func assertionErrTest(input string, t *testing.T) {
+	_, err := unit16Ranges(input)
+	if err == nil {
+		t.Fatalf("Did not get an error from input %s", input)
+	}
+}
+
+// Test a single invalid value
+func TestErrInvalidSingle(t *testing.T) {
+	assertionErrTest("x", t)
+}
+
+// Test a single out of range value on the high side
+func TestErrInvalidSingleOutOfRangeHigh(t *testing.T) {
+	assertionErrTest("65536", t)
+}
+
+// Test empty string
+func TestEmptyString(t *testing.T) {
+	assertionErrTest("", t)
+}
+
+// Test multiple values where an invalid is inside one of them
+func TestMultipleOneInvalid(t *testing.T) {
+	assertionErrTest("1,x,2", t)
+}
+
+// Test a completely invalid minimum for a range
+func TestInvalidMinimum(t *testing.T) {
+	assertionErrTest("x:10", t)
+}
+
+// Test a completely invalid maximum for a range
+func TestInvalidMaximum(t *testing.T) {
+	assertionErrTest("9:x", t)
+}
+
+// Test a single out of range value on the low side
+func TestErrInvalidSingleOutOfRangeLow(t *testing.T) {
+	assertionErrTest("-1", t)
+}
+
+// Test for one valid value at the minimum
+func TestUnit16RangesValidOneMin(t *testing.T) {
+	assertionNoErrTest("0", [][]uint16{{0}}, t)
+}
+
+// Test for one valid value at the maximum
+func TestUnit16RangesValidOneMax(t *testing.T) {
+	assertionNoErrTest("65535", [][]uint16{{65535}}, t)
+}
+
+// Test for one range covering all values
+func TestUnit16RangesValidAllValues(t *testing.T) {
+	assertionNoErrTest("0:65535", [][]uint16{{0, 65535}}, t)
+}
+
+// Test for two disjoint ranges
+func TestUnit16RangesDisjointRanges(t *testing.T) {
+	assertionNoErrTest("1:5,10:12", [][]uint16{{1, 5}, {10, 12}}, t)
+}
+
+// Test for two single values
+func TestUnit16RangesTwoValues(t *testing.T) {
+	assertionNoErrTest("22,80", [][]uint16{{22}, {80}}, t)
+}
