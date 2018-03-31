5bcbf2b7e4d30a753f47869282979d2f26ce2128
Patrick Santos <patrickceg@users.noreply.github.com>
2018-03-30 22:25:35 -0400

    Get the initial sending loop to work

--- bannergrabtroll.go
+++ bannergrabtroll.go
@@ -3,18 +3,99 @@ package main
 // Program that attempts to send back randomized payload to any TCP connection
 
 import (
+	"crypto/rand"
 	"flag"
 	"fmt"
+	"net"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 )
+
+// Loops indefinitely trying to start a connection on a port,
+// and if a connection is established, it hands that to handleConnection to
+// send garbage to that connection
+// The channel doesn't actually use values: it instead is just used as a
+// semaphore. We have run out of connections when sendersChannel's buffer is
+// full, and each time handleConnection finishes / fails, it takes from the buffer
+// The garbagePool is the 10k of pseudorandom data senders get to choose from
+func startConnectionListener(payloadKbytes int, rateKbytesPerConnection int,
+	sendersChannel chan bool, listener net.Listener) {
+	for {
+		conn, err := listener.Accept()
+		if err == nil {
+			// if a successful connection is formed, queue it up for the buffer by pushing to the channel
+			sendersChannel <- true
+			// when resources are available, start sending garbage to the connection
+			go handleConnection(conn, payloadKbytes, rateKbytesPerConnection, sendersChannel)
+		}
+	}
+}
+
+// Sends garbage to the given TCP connection until either the
+// payloadkbytes limit or the channel fails. It then signals back to the
+// 'done' channel.
+// (to only have up to 2k sent at a time)
+// (The channel is essentially used like a wait-notify rather than passing of data)
+// Do NOT reprogram this to use a UDP connection, or you've just built yourself a
+// distributed denial of service (DDoS) node!
+func handleConnection(tcpConnection net.Conn,
+	payloadKbytes int, rateKbytesPerConnection int, sendersChannel <-chan bool) {
+	// using 64-bit int because someone may be especially evil and troll with > 4 GiB of data
+	var currentBytes uint64 //= 0
+	maxBytes := uint64(payloadKbytes) * 1024
+	tenthOfSecond := time.Duration(100000)
+	bytesPerTenthOfSecond := uint32(rateKbytesPerConnection) / 10
+	// do the sending
+	var sendError error // = nil
+	for currentBytes < maxBytes && sendError == nil {
+		// start the timer
+		sendStartTime := time.Now()
+		// generate garbage to send
+		c := bytesPerTenthOfSecond
+		garbageSlice := make([]byte, c)
+		_, sendError := rand.Read(garbageSlice)
+		if sendError == nil {
+			// send the garbage if it generated properly
+			_, sendError = tcpConnection.Write(garbageSlice)
+			// check if we need to wait to throttle the sending
+			sendEndTime := time.Now()
+			timeSpentSending := sendEndTime.Sub(sendStartTime)
+			timeToSleep := tenthOfSecond - timeSpentSending
+			currentBytes = currentBytes + uint64(bytesPerTenthOfSecond)
+			if timeToSleep > 0 {
+				time.Sleep(timeToSleep)
+			}
+		}
+	}
+	// Log what garbage was sent
+	fmt.Printf("Sent %d bytes from %v to %v", currentBytes, tcpConnection.LocalAddr(), tcpConnection.RemoteAddr())
+	// free up resource by pulling from the channel
+	<-sendersChannel
+}
+
+// Add TCP listener of that port to the map
+// Prints a warning if the port is a duplicate or otherwise could not get opened
+func addConnectionToMap(port uint16, toAdd map[uint16]net.Listener) {
+	// Don't add duplicate connections
+	_, ok := toAdd[port]
+	if !ok {
+		portString := fmt.Sprintf(":%d", port)
+		ln, err := net.Listen("tcp", portString)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Could not open port %d: %v", port, err)
+			fmt.Fprintln(os.Stderr)
+		}
+		toAdd[port] = ln
+	}
+}
 
 // unit16Ranges takes the input string and encodes it as 2d slices of uint16
 // this function is intended for lists of ports
 // Each "row" (the first value) in the 2D slice is 1 or 2 elements: a 1 element
-// slice is for single numbers, wheras a 2 element slice is used for ranges, where
-// the first element is the minimum and the last element is the maximum
+// slice is for single numbers, wheras a 2 element slice is used for ranges,
+// where the first element is the minimum and the last element is the maximum
 func unit16Ranges(portString string) ([][]uint16, error) {
 	allLists := strings.Split(portString, ",")
 	var toReturn [][]uint16
@@ -50,49 +131,23 @@ func unit16Ranges(portString string) ([][]uint16, error) {
 	return toReturn, nil
 }
 
-func main() {
-	// Name of a file that will go in the folder this program runs from if the disclaimer is accepted
+// Initialize the array of 10k of random garbage
+// Returns back the error if there is any
+func createGarbagePool() ([10240]byte, error) {
+	c := 10240
+	garbagePoolSlice := make([]byte, c)
+	var garbagePoolArray [10240]byte
+	_, err := rand.Read(garbagePoolSlice)
+	if err != nil {
+		return garbagePoolArray, err
+	}
+	copy(garbagePoolArray[:], garbagePoolSlice[:])
+	return garbagePoolArray, nil
+}
+
+// Processes the disclaimer for the program
+func doDisclaimer() {
 	consentFileName := "iamcrazy"
-	notSpecifiedString := "NOT_SPECIFIED"
-	var portString string
-	flag.StringVar(&portString, "ports", notSpecifiedString, "Ports separated by comma, colon for ranges: 300:400,505 is for ports 300 to 400 plus port 505")
-	rateKbytesPerConnection := flag.Int("ratekbytesperconnection", 32, "Rate limit in kilobytes per second per connection")
-	numConnections := flag.Int("numconnections", 16, "Number of simultaneous connections across any ports")
-	flag.Parse()
-
-	if portString == notSpecifiedString {
-		fmt.Fprintln(os.Stderr, "Specify ports to listen on")
-		flag.PrintDefaults()
-		os.Exit(1)
-	}
-
-	// Get port ranges
-	portRanges, portRangeErr := unit16Ranges(portString)
-	if portRangeErr != nil {
-		fmt.Fprintf(os.Stderr, "Invalid port ranges %v", portRangeErr)
-		fmt.Fprintln(os.Stderr)
-		os.Exit(1)
-	}
-
-	// Validate other arguments are positive
-	payloadKbytes := flag.Int("payloadkbytes", 64, "How many kilobytes to send as payload to a connection")
-	if *payloadKbytes <= 0 {
-		fmt.Fprintf(os.Stderr, "Payload size must be postive: got %d", payloadKbytes)
-		fmt.Fprintln(os.Stderr)
-		os.Exit(1)
-	}
-	if *rateKbytesPerConnection <= 0 {
-		fmt.Fprintf(os.Stderr, "Rate per connection must be postive: got %d", rateKbytesPerConnection)
-		fmt.Fprintln(os.Stderr)
-		os.Exit(1)
-	}
-	if *numConnections <= 0 {
-		fmt.Fprintf(os.Stderr, "Number of connections must be postive: got %d", numConnections)
-		fmt.Fprintln(os.Stderr)
-		os.Exit(1)
-	}
-
-	// If user hasn't accepted disclaimer yet, print it and have the user type in a string that says they accept
 	if _, err := os.Stat(consentFileName); os.IsNotExist(err) {
 		var accepted = false
 		for !accepted {
@@ -117,10 +172,74 @@ func main() {
 			}
 		}
 	}
+}
+
+func main() {
+	// Name of a file that will go in the folder this program runs from if the disclaimer is accepted
+	notSpecifiedString := "NOT_SPECIFIED"
+	var portString string
+	flag.StringVar(&portString, "ports", notSpecifiedString, "Ports separated by comma, colon for ranges: 300:400,505 is for ports 300 to 400 plus port 505")
+	rateKbytesPerConnection := flag.Int("ratekbytesperconnection", 32, "Rate limit in kilobytes per second per connection")
+	maxConnections := flag.Int("maxConnections", 16, "Maximum number of simultaneous connections across any ports")
+	flag.Parse()
+
+	if portString == notSpecifiedString {
+		fmt.Fprintln(os.Stderr, "Specify ports to listen on")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	// Get port ranges
+	portRanges, portRangeErr := unit16Ranges(portString)
+	if portRangeErr != nil {
+		fmt.Fprintf(os.Stderr, "Invalid port ranges %v", portRangeErr)
+		fmt.Fprintln(os.Stderr)
+		os.Exit(1)
+	}
+
+	// Validate other arguments are positive
+	payloadKbytes := flag.Int("payloadkbytes", 64, "How many kilobytes to send as payload to a connection")
+	if *payloadKbytes <= 0 {
+		fmt.Fprintf(os.Stderr, "Payload size must be postive: got %d", *payloadKbytes)
+		fmt.Fprintln(os.Stderr)
+		os.Exit(1)
+	}
+	if *rateKbytesPerConnection <= 0 {
+		fmt.Fprintf(os.Stderr, "Rate per connection must be postive: got %d", *rateKbytesPerConnection)
+		fmt.Fprintln(os.Stderr)
+		os.Exit(1)
+	}
+	if *maxConnections <= 0 {
+		fmt.Fprintf(os.Stderr, "Number of connections must be postive: got %d", *maxConnections)
+		fmt.Fprintln(os.Stderr)
+		os.Exit(1)
+	}
+
+	// If user hasn't accepted disclaimer yet, print it and have the user type in a string that says they accept
+	doDisclaimer()
 
 	// Print user selection
-	fmt.Printf("Setting up traps on ports %v with payload size %d kbytes, data rate up to %d kbytes x %d", portRanges, *payloadKbytes, *rateKbytesPerConnection, *numConnections)
+	fmt.Printf("Setting up traps on ports %v with payload size %d kbytes, data rate up to %d kbytes x %d", portRanges, *payloadKbytes, *rateKbytesPerConnection, *maxConnections)
 	fmt.Println()
 
-	// TODO: Start TCP listener
+	var listenerMap = make(map[uint16]net.Listener)
+	for _, portRange := range portRanges {
+		if len(portRange) == 1 {
+			addConnectionToMap(portRange[0], listenerMap)
+		} else {
+			for portI := portRange[0]; portI <= portRange[1]; portI++ {
+				addConnectionToMap(portI, listenerMap)
+			}
+		}
+	}
+
+	sendersChannel := make(chan bool, *maxConnections)
+	for _, connection := range listenerMap {
+		go startConnectionListener(*payloadKbytes, *rateKbytesPerConnection, sendersChannel, connection)
+	}
+
+	for {
+		time.Sleep(time.Duration(1000000))
+	}
+
 }
